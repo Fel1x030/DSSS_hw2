9cdf5a1aff971b70a1ded33bf8f1dbf6d99fde2d
Fel1x030 <jiale030li@gmai.com>
2023-11-09 17:03:14 +0100

    commit tmq

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,30 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
+from math_quiz import getnum, getsym, prob_result
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_getnum(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+        for i in range(1000):  # Test a large number of random values
+            rand_num = getnum(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_getsym(self):
+        getsym() in {'+', '-', '*'}
 
-    def test_function_C(self):
+    def test_prob_result(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (3, 9, '*', '3 * 9', 27),
+                (3, 7, '-', '3 - 7', -4),
+                (7, 2, '-', '7 - 2', 5),
+                (5, 0, '*', '5 * 0', 0)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                prob_result(num1,num2,operator) == expected_problem, expected_answer
 
 if __name__ == "__main__":
     unittest.main()
