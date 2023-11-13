e47ef1c2d563659f911a99654a41867b5a32a90c
Fel1x030 <jiale030li@gmai.com>
2023-11-13 14:37:47 +0100

    commit init.py

---
+++ math_quiz/__init__.py
@@ -0,0 +1,17 @@
+'''
+from math_quiz import math_quiz
+from math_quiz import tests_math_quiz
+'''
+
+'''
+math_quiz.getnum()
+math_quiz.getsym()
+math_quiz.prob_result()
+tests_math_quiz.test_getnum(self)
+tests_math_quiz.test_getsym(self)
+tests_math_quiz.test_prob_result(self)
+'''
+
+
+from .math_quiz import *
+
