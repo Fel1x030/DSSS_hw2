481153a5b4400f59e5472fe4a813ed5d254ea21f
Fel1x030 <jiale030li@gmai.com>
2023-11-13 14:36:58 +0100

    commit setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Jiale",
+    author_email="jiale.li@fau.de",
+    packages=find_packages(),
+)
